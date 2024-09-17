71dc58de34c95d1a0c2a2ed4110c235fb1812b3a
Pratibha Singh <pratibhasinghh13@gmail.com>
2024-09-17 12:55:31 +0530

    Updated App.js

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Dashboard from './components/Dashboard';
 import ReportIssue from './components/ReportIssue';
 import Services from './components/Services';
 import Signup from './components/Signup';
 import TrackIssues from './components/TrackIssues';
 import Login from './components/Login';
+import CitizenDashboard from './components/CitizenDashboard';
+import LeaderDashboard from './components/LeaderDashboard';
 
 const App = () => {
   return (
@@ -17,7 +17,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/citizen-dashboard" element={<CitizenDashboard />} />
+        <Route path="/leader-dashboard" element={<LeaderDashboard />} />
         <Route path="/report" element={<ReportIssue />} />
         <Route path="/services" element={<Services />} />
         <Route path="/signup" element={<Signup />} />
